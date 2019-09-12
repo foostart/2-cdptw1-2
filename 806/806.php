6b9a23cca2c434dfe5db5f84c94bd1f633f300d7
foostart.com <foostart.com@gmail.com>
2019-09-12 07:47:34 +0700

    Commit

---
+++ 806/806.php
@@ -0,0 +1,38 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('./less/type-806.less', 'css/type-806.css');
+    
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+
+        <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/css/type-806.css">
+        <script src="<?php echo $url_path ?>/js/jquery-3.1.1.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/type-806.js"></script>
+    </head>
+
+    <body>
+        <?php include $dir_block.'/806-content.php'; ?>
+    </body>
+
+</html>

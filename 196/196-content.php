04429d0c343a38bf48f9078405cacb5956a69757
NguyenTheBao23205 <43943189+NguyenTheBao23205@users.noreply.github.com>
2019-09-22 18:36:45 +0700

    196

--- 196/196-content.php
+++ 196/196-content.php
@@ -1,53 +1,68 @@
-﻿
-<!DOCTYPE html>
-
-<html>
-
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">       
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
-        <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/364.less', 'css/364.css');
-        ?>
-        <link href="css/364.css" rel="stylesheet" type="text/css" />
-
-    </head>
-
-    <body>
-        <div class="type-364">
-            <div class="homepage_brands">
-                <div class="container">
-                    <h3 class="page_heading">BRANDS</h3>
-                    <div class="row" style=" padding-left: 15px; line-height: 8;">  
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand1_img.jpg" alt=""></a>
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<!-- Swiper -->
+<div class="type-196">
+    <div class="banner">
+        <div class="container">
+            <div class="swiper-container">
+                <h3>BRANDS
+                </h3>
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="block">
+                            <div class="image">
+                                <img src="images/custom_brand1_img.jpg" alt=""/>
+                            </div>
                         </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand2_img.jpg" alt=""></a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="block">
+                            <div class="image">
+                                <img src="images/custom_brand2_img.jpg" alt=""/>
+                            </div>
                         </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand3_img.jpg" alt=""></a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="block">
+                            <div class="image">
+                                <img src="images/custom_brand3_img.jpg" alt=""/>
+                            </div>
                         </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand4_img.jpg" alt=""></a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="block">
+                            <div class="image">
+                                <img src="images/custom_brand4_img.jpg" alt=""/>
+                            </div>
                         </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand5_img.jpg" alt=""></a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="block">
+                            <div class="image">
+                                <img src="images/custom_brand5_img.jpg" alt=""/>
+                            </div>
                         </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand6_img.jpg" alt=""></a>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="block">
+                            <div class="image">
+                                <img src="images/custom_brand6_img.jpg" alt=""/>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="swiper-pagination"></div>
+                <!-- Add Arrows -->
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
             </div>
         </div>
-    </body>
-</html>
+    </div>   
+</div>
+
--- 196/196.php
+++ 196/196.php
@@ -1,53 +1,32 @@
-﻿
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/type-196.less', 'css/type-196.css');
+?>
 <!DOCTYPE html>
-
-<html>
-
+<html lang="en">
     <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">       
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
-        <?php
-        if (!class_exists('lessc')) {
-            include ('./libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/364.less', 'css/364.css');
-        ?>
-        <link href="css/364.css" rel="stylesheet" type="text/css" />
-
+        <title>196</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/type-196.css" rel="stylesheet" type="text/css" />
+        <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap.min.js" ></script>  
+        <script src="<?php echo $url_path ?>/js/swiper.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/196.js"></script>
     </head>
-
     <body>
-        <div class="type-364">
-            <div class="homepage_brands">
-                <div class="container">
-                    <h3 class="page_heading">BRANDS</h3>
-                    <div class="row" style=" padding-left: 15px; line-height: 8;">  
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand1_img.jpg" alt=""></a>
-                        </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand2_img.jpg" alt=""></a>
-                        </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand3_img.jpg" alt=""></a>
-                        </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand4_img.jpg" alt=""></a>
-                        </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand5_img.jpg" alt=""></a>
-                        </div>
-                        <div class="col-md-2 col-sm-4 col-xs-6">
-                            <a href="#"><img src="images/custom_brand6_img.jpg" alt=""></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include $dir_block . '/196-content.php'; ?>
     </body>
 </html>
